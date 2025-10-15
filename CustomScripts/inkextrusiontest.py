9d0b54f66da46c0ef08d94b1bffcefa4dcc1cf3e
Dwarakesh Rajesh <dwara.rajesh@gmail.com>
2025-10-15 17:30:27 -0400

    commit

--- CustomScripts/inkextrusiontest.py
+++ CustomScripts/inkextrusiontest.py
@@ -1,10 +1,8 @@
 from coreModule import *
 
-time.sleep(5) #hold for 5 seconds
-set_pressure(45) #set pressure to 45 PSI
-ink_on() #turn on ink solenoid
-time.sleep(10) #wait for 10s 
-ink_off() #switch off ink
-
-#Fresh ink works at 45 PSI
-#One day later - Capped and stored doesn't work at 85 PSI, and 100 PSI
+time.sleep(5)
+set_pressure(80)
+time.sleep(2)
+ink_on()
+time.sleep(10)
+ink_off()
