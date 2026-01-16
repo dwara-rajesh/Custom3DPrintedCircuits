9e2ac2c8698ee4955c378a43e3ada7f7e000508b
Dwarakesh Rajesh <dwara.rajesh@gmail.com>
2026-01-16 18:24:33 -0500

    restructure

--- CustomScripts/getrobotpos.py
+++
@@ -1,3 +0,0 @@
-import rtde_receive
-rtde_receive = rtde_receive.RTDEReceiveInterface("10.241.34.45")
-print(rtde_receive.getActualTCPPose())
--- CustomScripts/inkextrusiontest.py
+++
@@ -1,8 +0,0 @@
-from coreModule import *
-
-time.sleep(5)
-set_pressure(80)
-time.sleep(2)
-ink_on()
-time.sleep(10)
-ink_off()
